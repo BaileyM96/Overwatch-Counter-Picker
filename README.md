54b6433e34d7e0be8fbe5889019ee3b29c3874ff
Bailey Mejia <baileymejia28@gmail.com>
2022-11-17 18:06:21 -0800

    Added JSON packages, index.js , and starting code.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Overwatch-Counter-Picker
+
+## Description
+This is a little project for me, as someone that loves playing overwatch I wanted to create an application that will help you  pick a character that can help you defeat a character that is making the match miserable for you and your team. This will help you counter team comps and maybe very well help you win that next match to get out of gold.
---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const overwatchDpsAgents = [
+    'Ashe', 'Bastion', 'Echo', 'Genji', 'Hanzo', 'Junkrat', 'Cassidy', 'Mei', 'Pharah', 'Reaper', 'Sojourn', 'Soldier 76', 'Sombra', 'Symmetra', 'Torbjorn', 'Tracer', 'Widowmaker'
+];
+
+const overwatchSupportAgents = [
+    'Ana', 'Baptiste', 'Brigitte', 'Kiriko', 'Lucio', 'Mercy', 'Moira', 'Zenyatta'
+];
+
+const overwatchTankAgents = [
+    'D.Va', 'Doomfist', 'Junker Queen', 'Orisa', 'Reinhardt', 'Roadhog', 'Sigma', 'Winston', 'Wrecking Ball', 'Zarya'
+];
